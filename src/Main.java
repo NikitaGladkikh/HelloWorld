667634d0bda991660b0992c2015e39059e2ac5cf
Nikita Gladkikh <Mikita_Hladkikh@epam.com>
2014-08-15 15:56:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ёёёёё!");
-        System.out.println("ёёё!");
+        System.out.println("rfhf!");
+        System.out.println("fdssa!");
     }
 }
